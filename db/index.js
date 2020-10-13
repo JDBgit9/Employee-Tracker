fa6c31e7197f9a4b3d75c3d308370366d755c66b
JDBgit9 <info@jamesdbeeks.com>
2020-10-12 23:20:46 -0400

    this is my first of many commits been researching and learning and now ready to apply. I have the dbeaver database in working order and now working on the front node js

---
+++ db/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mysql = require("mysql");
+const inquirer = require('inquirer');
+
+// Define the MySQL connection parameters
+
+  // Create DB
+
+// app.get("/createdb", (req, res) => {
+
+// 	let sql = "CREATE DATABASE nodemysql";
+  
+// 	db.query(sql, (err) => {
+  
+// 	  if (err) {
+  
+// 		throw err;
+  
+// 	  }
+  
+// 	  res.send("Database created");
+  
+// 	});
+  
+//   });
+  
+  
+  
+  
+  
+
+// const addRole = () => {
+// 	inquirer
+// 	.prompt({
+// 		type:
+// 		"input",
+// 		name: addRole,
+// 		message
+// 	})
+// }
+// Alter this file as needed to match our requirements
+// var mysql = require('mysql');
+// var inquirer = require('inquirer');
+// Export the connection so that we may use this in other parts of our program
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "rootroot",
+//     database: "employees"
+//   });
+  
+  // connect to the mysql server and sql database
+//   connection.connect(function(err) {
+//     if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    // start();
+//   });
+//   connection.changeUser({user : 'john'}, function(err) {
+//     if (err) throw err;
+//   });
+//   connection.query('SELECT * FROM `books` WHERE `author` = "David"', function (error, results, fields) {
+//     // error will be an Error if one occurred during the query
+//     // results will contain the results of the query
+//     // fields will contain information about the returned results fields (if any)
+//   });

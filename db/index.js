cc63cdad54c9e3b2ed0b2547f613c70cf97ca541
JDBgit9 <info@jamesdbeeks.com>
2020-10-14 22:14:32 -0400

    everything is working oreder my index.js has the proper functions and calls to add department add role view department, roles, and employees

--- db/index.js
+++ db/index.js
@@ -102,7 +102,75 @@ function prompt() {
 			}
 		});
 }
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "department",
+            type: "input",
+            message: "What is the department name?",
+          })
+        .then(function(answer) {
+        var query = "INSERT INTO department (name) VALUES ( ? )";
+        connection.query(query, answer.department, function(err, res) {
+            console.log(`You have added this department: ${(answer.department).toUpperCase()}.`)
+        })
+        prompt();
+        })
+}
 
+function addRole() {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw (err);
+    inquirer
+        .prompt([{
+            name: "title",
+            type: "input",
+            message: "What is the role title?",
+          }, 
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of the new role?",
+          },
+          {
+            name: "departmentName",
+            type: "list",
+// is there a way to make the options here the results of a query that selects all departments?`
+            message: "Which department does this role fall under?",
+            choices: function() {
+                var choicesArray = [];
+                res.forEach(res => {
+                    choicesArray.push(
+                        res.name
+                    );
+                })
+                return choicesArray;
+              }
+          }
+          ]) 
+// in order to get the id here, i need a way to grab it from the departments table 
+        .then(function(answer) {
+        const department = answer.departmentName;
+        connection.query('SELECT * FROM DEPARTMENT', function(err, res) {
+        
+            if (err) throw (err);
+         let filteredDept = res.filter(function(res) {
+            return res.name == department;
+        }
+        )
+        let id = filteredDept[0].id;
+       let query = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+       let values = [answer.title, parseInt(answer.salary), id]
+       console.log(values);
+        connection.query(query, values,
+            function(err, res, fields) {
+            console.log(`You have added this role: ${(values[0]).toUpperCase()}.`)
+        })
+            viewRoles()
+            })
+        })
+    })
+}
 function viewEmployees() {
 	const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
     FROM employee

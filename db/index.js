bc808d3d02ceea231540502ae8ab9647a050bf68
JDBgit9 <info@jamesdbeeks.com>
2020-10-14 22:25:01 -0400

    final touches

--- db/index.js
+++ db/index.js
@@ -1,10 +1,7 @@
 'use strict';
-
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
-
-
 
 const promptMessages = {
 	addDepartment: "Add Department",
@@ -22,14 +19,8 @@ const promptMessages = {
 
 const connection = mysql.createConnection({
 	host: 'localhost',
-
-	// Your port; if not 3306
 	port: 3306,
-
-	// Your username
 	user: 'root',
-
-	// Your password
 	password: 'rootroot',
 	database: 'employees'
 });
